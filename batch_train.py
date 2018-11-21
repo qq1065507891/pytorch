538010c3808a04a9a1defc487f94ab39f4544ab9
qq1065507891 <42400109+qq1065507891@users.noreply.github.com>
2018-11-21 20:08:48 +0800

    Update batch_train.py

--- batch_train.py
+++ batch_train.py
@@ -8,19 +8,21 @@ BATCH_SIZE = 5
 x = torch.linspace(1, 10, 10)
 y = torch.linspace(10, 1, 10)
 
-torch_dataset = Data.TensorDataset(data_tensor=x, target_tensor=y)
-loader = Data.DataLoder(
+torch_dataset = Data.TensorDataset(x, y)
+loader = Data.DataLoader(
     dataset=torch_dataset,
-    batch_size = BATCH_SIZE,
-    shuffle = True,
-    num_worker=2,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    num_workers=2,
 )
+
 
 def show_batch():
     for epoch in range(3):
         for step, (batch_x, batch_y) in enumerate(loader):
             print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
                   batch_x.numpy(), '| batch y: ', batch_y.numpy())
+
 
 if __name__ == "__main__":
     show_batch()
